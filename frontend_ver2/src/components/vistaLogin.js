96ceb759bb03fa466ad81b8fe81d31cc274041ec
Axel Bulfon <axelbulfon91@gmail.com>
2020-10-01 01:59:57 -0300

    Agregado estilos en paginas de login y registro

--- frontend_ver2/src/components/vistaLogin.js
+++ frontend_ver2/src/components/vistaLogin.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react'
 import GoogleLogin from 'react-google-login';
 import Axios from 'axios';
 import { Container, Button } from "react-bootstrap";
+import Footer from '../components/comercio/uso_compartido/Footer';
+import SeccionContacto from '../components/comercio/SeccionContacto';
+import estilos from '../assets/css/VistaPerfil.module.css'
+import BarraNavegacion from '../components/comercio/uso_compartido/BarraNavegacion';
 
 function VistaLogin() {
     const [usuario, setUsuario] = useState("")
@@ -71,50 +75,56 @@ function VistaLogin() {
 
     return (
         <React.Fragment>
+            <div className={estilos.fondo} style={{ paddingBottom: "3em" }}>
+                <div className={estilos.fondoBarra}></div>
+                <BarraNavegacion></BarraNavegacion>
+                <Container className="mt-5">
+                    <h3 className="text-center text-dark display-4">Login</h3>
+                    <div className="row justify-content-center mt-5">
+                        <div className="col-md-6">
+                            <form onSubmit={(e) => handleOnSubmit(e)}>
+                                <div className="form-group text-left">
+                                    <label>Email address</label>
+                                    <input type="email"
+                                        value={usuario}
+                                        className="form-control"
+                                        placeholder="Enter email"
+                                        onChange={(e) => setUsuario(e.target.value)}
+                                    />
+                                </div>
+                                <div className="form-group text-left">
+                                    <label >Password</label>
+                                    <input type="password"
+                                        className="form-control"
+                                        placeholder="Password"
+                                        value={contrasenia}
+                                        onChange={(e) => setContrasenia(e.target.value)}
+                                    />
+                                </div>
 
-            <Container className="mt-5">
-                <h2>Login</h2>
-                <div className="row">
-                    <div className="col-md-6">
-                        <form onSubmit={(e) => handleOnSubmit(e)}>
-                            <div className="form-group text-left">
-                                <label>Email address</label>
-                                <input type="email"
-                                    value={usuario}
-                                    className="form-control"
-                                    placeholder="Enter email"
-                                    onChange={(e) => setUsuario(e.target.value)}
-                                />
-                            </div>
-                            <div className="form-group text-left">
-                                <label >Password</label>
-                                <input type="password"
-                                    className="form-control"
-                                    placeholder="Password"
-                                    value={contrasenia}
-                                    onChange={(e) => setContrasenia(e.target.value)}
-                                />
-                            </div>
-
-                            <button type="submit" className="btn btn-primary mr-3">Ingresar</button>
+                                <button type="submit" className="btn btn-primary mr-3">Ingresar</button>
                             Sin cuenta? <a href="/registro" className="text-primary">Registrate</a>
-                            <Button className="d-flex float-right btn btn-sm btn-warning" onClick={() => enviarEmail()}>Olvide mi contraseña</Button>
-                        </form>
+                                <Button className="d-flex float-right btn btn-sm btn-warning" onClick={() => enviarEmail()}>Olvide mi contraseña</Button>
+                            </form>
 
-                        <br />
-                    o ingresa por Google
-                        <GoogleLogin
-                            className="ml-4"
-                            clientId={process.env.REACT_APP_GOOGLE_LOGIN_CLIENTE_ID}
-                            buttonText="Ingresar con Google"
-                            onSuccess={loginCorrectoGoogle}
-                            onFailure={loginIncorrectoGoogle}
-                            cookiePolicy={'single_host_origin'}
-                        />
+                            <br />
+                            <div className="text-center">
+                                o ingresa por Google
+                                <GoogleLogin
+                                    className="ml-4"
+                                    clientId={process.env.REACT_APP_GOOGLE_LOGIN_CLIENTE_ID}
+                                    buttonText="Ingresar con Google"
+                                    onSuccess={loginCorrectoGoogle}
+                                    onFailure={loginIncorrectoGoogle}
+                                    cookiePolicy={'single_host_origin'}
+                                />
+                            </div>
+                        </div>
                     </div>
-                </div>
-
-            </Container>
+                </Container>
+            </div>
+            <SeccionContacto></SeccionContacto>
+            <Footer></Footer>
         </React.Fragment>
     )
 }
--- frontend_ver2/src/components/vistaRegistro.js
+++ frontend_ver2/src/components/vistaRegistro.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react'
 import GoogleLogin from 'react-google-login';
 import Axios from 'axios';
+import Footer from '../components/comercio/uso_compartido/Footer';
+import SeccionContacto from '../components/comercio/SeccionContacto';
+import estilos from '../assets/css/VistaPerfil.module.css'
+import BarraNavegacion from '../components/comercio/uso_compartido/BarraNavegacion';
 
 import { Container } from "react-bootstrap";
 
@@ -11,7 +15,7 @@ function VistaRegistro() {
     const [contrasenia2, setContrasenia2] = useState("")
 
     const registroLocal = async () => {
-        
+
         if (usuario !== "") {
             if (contrasenia === contrasenia2 && contrasenia.length >= 6) {
                 const user = {
@@ -31,7 +35,7 @@ function VistaRegistro() {
             } else {
                 alert("Las contraseñas ingresadas no coinciden o tienen menos de 6 caracteres")
             }
-        }else{
+        } else {
             alert("El campo de correo electronico no puede estar vacio")
         }
     }
@@ -61,56 +65,63 @@ function VistaRegistro() {
 
     return (
         <React.Fragment>
+            <div className={estilos.fondo} style={{ paddingBottom: "3em" }}>
+                <div className={estilos.fondoBarra}></div>
+                <BarraNavegacion></BarraNavegacion>
+                <Container className="mt-5">
+                    <h3 className="text-center text-dark display-4">Registro</h3>
+                    <div className="row justify-content-center mt-5">
+                        <div className="col-md-6">
+                            <div className="form-group text-left">
+                                <label>Email address</label>
+                                <input type="email"
+                                    value={usuario}
+                                    className="form-control"
+                                    placeholder="Enter email"
+                                    onChange={(e) => setUsuario(e.target.value)}
+                                />
+                            </div>
+                            <div className="form-group text-left">
+                                <label >Password</label>
+                                <input type="password"
+                                    className="form-control"
+                                    placeholder="Password"
+                                    value={contrasenia}
+                                    onChange={(e) => setContrasenia(e.target.value)}
+                                />
+                            </div>
+                            <div className="form-group text-left">
+                                <label >Password</label>
+                                <input type="password"
+                                    className="form-control"
+                                    placeholder="Repeat password"
+                                    value={contrasenia2}
+                                    onChange={(e) => setContrasenia2(e.target.value)}
+                                />
+                            </div>
+                            <div className="text-center">
+                                <button onClick={() => registroLocal()} className="btn btn-primary mr-3">Registrarse</button><br /><br />
+                                Tienes cuenta? <a href="/login" className="text-primary">Ingresa</a>
+                            </div>
+                            <br />
+                            <div className="text-center">
+                                o registrate por Google
+                                <GoogleLogin
+                                    className="ml-4"
+                                    clientId={process.env.REACT_APP_GOOGLE_LOGIN_CLIENTE_ID}
+                                    buttonText="Ingresar con Google"
+                                    onSuccess={registroCorrecto}
+                                    onFailure={registroIncorrecto}
+                                    cookiePolicy={'single_host_origin'}
+                                />
+                            </div>
 
-            <Container className="mt-5">
-                <h2>Registro</h2>
-                <div className="row">
-                    <div className="col-md-6">
-                        <div className="form-group text-left">
-                            <label>Email address</label>
-                            <input type="email"
-                                value={usuario}
-                                className="form-control"
-                                placeholder="Enter email"
-                                onChange={(e) => setUsuario(e.target.value)}
-                            />
                         </div>
-                        <div className="form-group text-left">
-                            <label >Password</label>
-                            <input type="password"
-                                className="form-control"
-                                placeholder="Password"
-                                value={contrasenia}
-                                onChange={(e) => setContrasenia(e.target.value)}
-                            />
-                        </div>
-                        <div className="form-group text-left">
-                            <label >Password</label>
-                            <input type="password"
-                                className="form-control"
-                                placeholder="Repeat password"
-                                value={contrasenia2}
-                                onChange={(e) => setContrasenia2(e.target.value)}
-                            />
-                        </div>
-
-                        <button onClick={() => registroLocal()} className="btn btn-primary mr-3">Registrarse</button>
-                        Tienes cuenta? <a href="/login" className="text-primary">Ingresa</a>
-                        <br />
-
-                    o registrate por Google
-
-                    <GoogleLogin
-                            className="ml-4"
-                            clientId={process.env.REACT_APP_GOOGLE_LOGIN_CLIENTE_ID}
-                            buttonText="Ingresar con Google"
-                            onSuccess={registroCorrecto}
-                            onFailure={registroIncorrecto}
-                            cookiePolicy={'single_host_origin'}
-                        />
                     </div>
-                </div>
-            </Container>
+                </Container>
+            </div>
+            <SeccionContacto></SeccionContacto>
+            <Footer></Footer>
         </React.Fragment>
     )
 }
